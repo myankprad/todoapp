2603b371773d7cc53e228ee09e455e77aff78635
[mayank pradhan] <mayankpradhan115@gmail.com>
2023-03-23 20:18:59 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./css/style.css"
+import Displaytodo from './component/Displaytodo';
 import Todos from "./component/Todos" 
 function App() {
   return (
     <div className="App">
+    <h1>Keep Notes</h1>
       <Todos />
+      <Displaytodo />
     </div>
   );
 }
